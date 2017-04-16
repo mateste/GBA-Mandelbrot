0e57088bfd6af3125299fd0244ea9ce3a2128807
Mateusz Stepien <mateusz@argc.pl>
2017-04-16 22:32:58 +0200

    basic Mandelbrot plotting
    dog slow btw, whole plot is done in over a minute

--- main.cpp
+++ main.cpp
@@ -2,25 +2,56 @@
 #include <random>
 #include <limits>
 
+float getPixelCoordsX(u32 x)
+{
+    auto minX = -2.5f;
+    auto maxX = 1.f;
+
+    auto result = (static_cast<float>(x) / static_cast<float>(SCREEN_WIDTH) * (maxX - minX)) + minX;
+    return result;
+}
+
+float getPixelCoordsY(u32 y)
+{
+    auto minY = -1.f;
+    auto maxY = 1.f;
+
+    auto result = (static_cast<float>(y) / static_cast<float>(SCREEN_HEIGHT) * (maxY - minY)) + minY;
+    return result;
+}
+
+void mandelbrot()
+{
+    for(auto i = 0u; i < SCREEN_HEIGHT; i++)
+    {
+        for(auto j = 0u; j < SCREEN_WIDTH; j++)
+        {
+            auto x0 = getPixelCoordsX(j);
+            auto y0 = getPixelCoordsY(i);
+            auto x = 0.f;
+            auto y = 0.f;
+            auto iteration = 0u;
+            for(iteration; iteration < 62u && x*x + y*y < 4; iteration++)
+            {
+                auto xtemp = x*x - y*y + x0;
+                y = 2*x*y + y0;
+                x = xtemp;
+            }
+            MODE3_FB[i][j] = RGB5(31 - (iteration / 2), 0, 0);
+        }
+    }
+}
+
 int main()
 {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<u16> dis;
-    std::uniform_int_distribution<u32> widthDis(0, SCREEN_WIDTH);
-    std::uniform_int_distribution<u32> heightDis(0, SCREEN_HEIGHT);
-
     irqInit();
     irqEnable(IRQ_VBLANK);
     SetMode(MODE_3 | BG2_ON);
 
+    mandelbrot();
     while(1)
     {
         VBlankIntrWait();
-        for(auto i = 0u; i < 1000; i++)
-        {
-            MODE3_FB[heightDis(gen)][widthDis(gen)] = dis(gen);
-        }
     }
 
     return 0;
